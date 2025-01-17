dc1e6722dcdbe7812c6ade00b55e822682350873
Orfeas Zervakis <sdi2400050@di.uoa.gr>
2025-01-17 17:08:15 +0200

    2nd attempt at fixing the the code

--- future/src/future.c
+++ future/src/future.c
@@ -1,23 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define H 1000000000
 
 //values.txt
-        //fscanf(file, "%f", &N[j]);
-        // = malloc( 100001 * sizeof(float))
+        //
+        //
 int main(){
+    float *N = malloc(H * sizeof(float));
+    printf("hi\n");
+    if(N==NULL){
+        fprintf(stderr, "error\n");
+        return 1;
+    }
+    printf("hi\n");
+    char *M;
+    printf("hi\n");
+    scanf("%s", M);
+    printf("hi\n");
     FILE *file = fopen("test69.txt", "r");
-    float *N;
+    printf("hi\n");
+    
     int j = 1;
-    int i = 1;
+    printf("hi\n");
+    int i = 0;
+    printf("hi\n");
     while( j > 0){
+        printf("hi\n");
+        fscanf(file, "%f", &N[j]);
+        printf("hi\n");
         if(fscanf(file, "%f", &N[j]) != EOF){
-            printf("hi");
+            printf("hi\n");
             j++;
+            printf("hi\n");
             i++;
+            printf("hi\n");
         }
         else{
+            printf("hi\n");
             j = 0;
         }
-
+        printf("hi\n");
     }
+    printf("hi\n");
+    free(N);
+    fclose(file);
 }
